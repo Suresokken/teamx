d27bde3c6ae3cff8ca00a133837d85a6618f1262
Suresokken <jigzie@gmail.com>
2019-03-05 17:59:56 +0100

    [Tekst] tekst tilføjet

--- src/components/main/stoltIndex.tsx
+++ src/components/main/stoltIndex.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import bottle from '../../assets/bottle.jpg';
 import wines from '../../assets/wines.png';
 
-
 class Stolt extends React.Component {
     public render() {
         return (
@@ -25,6 +24,16 @@ class Stolt extends React.Component {
                                     Vi går ikke på kompromis med kvalitet, og er kompromisløse i vores jagt på
                                     perfektion.</p>
                                 <hr/>
+                                <p className="text-left">Collazzi Winery ligger i smukke omgivelser lige udenfor
+                                    Firenze i Toscana sammen med en olivenlund med 19.000 oliventræer, en citronlund
+                                    og adskillige vinmarker på skråninger og terrasser i området.<br/>
+                                    Produkterne fra Collazzi er i særklasse og af højeste kvalitet, hvor
+                                    kældermesteren er kendt for hellere at hælde store mængder vin ud end at sælge
+                                    sekundær kvalitet.<br/>
+                                    Hovedhuset ”The Collazzi Villa” er opført over en 16 års periode i det 16.
+                                    århundrede og både møbler er designet og malerierne i huset tegnet af Michel
+                                    Angelo. Stolten-Holm sælger produkterne fra Collazzi eksklusivt i Danmark.
+                                </p>
                             </div>
                             <div className="col-2 text-center">
                                 <img className="imgBottle" src={bottle}/>
@@ -33,7 +42,7 @@ class Stolt extends React.Component {
                     </div>
                 </div>
                 <div className="container jumbAreaThree text-center">
-                <img className="img-fluid" src={wines} width=""/>
+                    <img className="img-fluid" src={wines} width=""/>
                     <div>
                         <h5>STOLTEN-HOLM VIN</h5>
                         <p>info@stolten-holm.dk<br/>+45 27 81 60 78</p>
